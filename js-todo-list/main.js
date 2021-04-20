77966f96c9d5c813402d7af7b146c1b5714f80ac
TANG <tyu4520@gmail.com>
2021-04-20 16:17:41 +0800

    complete new task

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -2,10 +2,25 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('ul').addEventListener('click', function(e){
     e.target.classList.toggle('checked')
   })
-  const closeArr = document.querySelectorAll('span')
+  const closeArr = document.querySelectorAll('.close')
   closeArr.forEach(function(e){
     e.addEventListener('click', function(e){
       e.target.parentElement.remove()
     }) 
   })
+  document.querySelector('#addBtn').addEventListener('click', function(){
+    let input = document.querySelector('#input').value
+    const newLi = document.createElement('li')
+    newLi.textContent = input
+    const newSpan = document.createElement('span')
+    // console.log(newSpan)
+    newSpan.classList.add('close')
+    newSpan.innerHTML = 'x'
+    newLi.appendChild(newSpan)
+    document.querySelector('ul').appendChild(newLi)
+    newSpan.addEventListener('click', function(){
+      newSpan.parentElement.remove()
+    })
+  })
 })
+
