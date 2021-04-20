f1387fe38b44d03e44073adb5b153d2ee68215fa
TANG <tyu4520@gmail.com>
2021-04-20 14:07:04 +0800

    11

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('ul').addEventListener('click', function(e){
-    // debugger
     e.target.classList.toggle('checked')
   })
+  
 })
