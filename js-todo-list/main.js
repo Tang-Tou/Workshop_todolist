d919108d45729a0075144dc3f9529997f3d05b10
TANG <tyu4520@gmail.com>
2021-04-20 14:09:58 +0800

    complete x_task

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -2,5 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('ul').addEventListener('click', function(e){
     e.target.classList.toggle('checked')
   })
-  
+  const closeArr = document.querySelectorAll('span')
+  closeArr.forEach(function(e){
+    e.addEventListener('click', function(e){
+      e.target.parentElement.remove()
+    }) 
+  })
 })
